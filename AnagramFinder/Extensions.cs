1a44789face333122fdd94955d42a293646ec5f7
Владислав <wertta07@mail.ru>
2020-11-12 12:13:00 +0300

    Add project files.

---
+++ AnagramFinder/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnagramFinder
+{
+    public static class Extensions
+    {
+        public static IEnumerable<string[]> GroupByAnagram(this string[] strings)
+        {
+            var anagramGroups = strings
+                    .Select(Selector)
+                    .GroupBy(tuple => tuple.asciiSum)
+                ;
+            foreach (var group in anagramGroups)
+            {
+                yield return group
+                    .Select(tuple => tuple.value)
+                    .ToArray();
+            }
+        }
+
+        private static (int asciiSum, string value) Selector(string value) =>
+            (value
+                    .ToLower()
+                    .Select(Convert.ToInt32)
+                    .Sum(),
+                value);
+    }
+}
---
+++ AnagramFinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AnagramFinder
+{
+    class Program
+    {
+        private static string[] _input =
+        {
+            "ток", "рост", "кот", "торс", "Кто", "фывап", "рок"
+        };
+
+        static void Main(string[] args)
+        {
+            foreach (var strings in _input.GroupByAnagram())
+            {
+                Console.WriteLine(string.Join(",", strings));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
